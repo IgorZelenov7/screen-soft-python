588c42861660f135e9933e3d39f9581df77503d8
Igor Zelenov <izelenov7@mail.ru>
2025-01-25 15:24:07 +0300

    add button

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from tkinter import messagebox
 
 root = Tk()
 root.title('Screen Soft')
-root.geometry('200x40')
+root.geometry('300x70')
 root.resizable(width=False, height=False)
+
+
+btn = Button(root, text='Screen', font=('Comic Sans MS', 15, 'bold'))
+btn.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 
 
