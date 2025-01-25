edafcc265ffd2a737357a02db460cd71423f0e29
Igor Zelenov <izelenov7@mail.ru>
2025-01-25 16:11:24 +0300

    Ubuntu font for button

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ root.geometry('300x70')
 root.resizable(width=False, height=False)
 
 # Кнопка для создания скриншота
-btn = Button(root, text='Screen', font=('Comic Sans MS', 15, 'bold'), command=screen)
+btn = Button(root, text='Screen', font=('Ubuntu Regular 400', 15, 'bold'), command=screen)
 btn.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 
