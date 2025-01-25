3d23331c4883365090d9d44d37979aa366402d4f
Igor Zelenov <izelenov7@mail.ru>
2025-01-25 15:48:54 +0300

    beta version (work)

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from pyautogui import *
+from pyautogui import screenshot
 from tkinter import *
 from tkinter import messagebox
 
+
+def screen():
+    img = screenshot() # Создаем скриншот
+    img.save(r'screen.png')  # Сохраняем скриншот
+    messagebox.showinfo('Alert', 'Screen Ready')  # Показываем сообщение
+
+
+# Графический интерфейс
 root = Tk()
 root.title('Screen Soft')
 root.geometry('300x70')
 root.resizable(width=False, height=False)
 
-
-btn = Button(root, text='Screen', font=('Comic Sans MS', 15, 'bold'))
+# Кнопка для создания скриншота
+btn = Button(root, text='Screen', font=('Comic Sans MS', 15, 'bold'), command=screen)
 btn.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 
