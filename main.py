8830145559b6bf6b0df1b4c1ebeba0ddb6c98cf0
Igor Zelenov <izelenov7@mail.ru>
2025-01-25 15:19:21 +0300

    add window

---
+++ main.py
@@ -0,0 +1,12 @@
+from pyautogui import *
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.title('Screen Soft')
+root.geometry('200x40')
+root.resizable(width=False, height=False)
+
+
+
+root.mainloop()
